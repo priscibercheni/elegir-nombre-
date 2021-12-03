ce1cfeb6300943e27ad564615b5029542f2a66c3
priscibercheni <prisci.bercheni@hotmail.com>
2021-12-03 19:25:04 -0300

    Commit de prueba

---
+++ g5_BercheñiPriscila_D1.py
@@ -0,0 +1,7 @@
+edad=int(input("edad de la persona: "))
+nombre=input("nombre de la persona: ")
+
+if edad > 16:
+    print(nombre + " puede votar")
+else:
+    print (nombre + " debe esperar para votar") 
